8a4f23abd6033a73ecd1737f9187db7abed9b13c
KushalDey02 <kushaldey02@gmail.com>
2025-08-14 17:46:52 +0530

    Update conf.py

--- content/conf.py
+++ content/conf.py
@@ -1,19 +1,8 @@
 # Configuration file for the Sphinx documentation builder.
-#
-# This file only contains a selection of the most common options. For a full
-# list see the documentation:
-# https://www.sphinx-doc.org/en/master/usage/configuration.html
 
-# -- Path setup --------------------------------------------------------------
-
-# If extensions (or modules to document with autodoc) are in another directory,
-# add these directories to sys.path here. If the directory is relative to the
-# documentation root, use os.path.abspath to make it absolute, like shown here.
-#
 import os
 import sys
 sys.path.insert(0, os.path.abspath('.'))
-
 
 # -- Project information -----------------------------------------------------
 
@@ -23,32 +12,26 @@ author = "Kushal Dey"
 github_user = "KushalDey02"
 github_repo_name = "CONAN1"  # auto-detected from dirname if blank
 github_version = "main"
-conf_py_path = "/content/" # with leading and trailing slash
+conf_py_path = "/content/"  # with leading and trailing slash
 
 # -- General configuration ---------------------------------------------------
 
-# Add any Sphinx extension module names here, as strings. They can be
-# extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
-# ones.
 extensions = [
-    # githubpages just adds a .nojekyll file
+    # GitHub pages
     "sphinx.ext.githubpages",
+    # CodeRefinery lesson structure
     "sphinx_lesson",
-    # remove once sphinx_rtd_theme updated for contrast and accessibility:
+    # Theme accessibility fixes
     "sphinx_rtd_theme_ext_color_contrast",
     "sphinx_coderefinery_branding",
-    # Math equations
+    # Math support
     "sphinx.ext.mathjax",
+    # MyST Markdown parser
+    "myst_parser",
 ]
 
 nb_execution_mode = "cache"
 
-# Add any paths that contain templates here, relative to this directory.
-# templates_path = ['_templates']
-
-# List of patterns, relative to source directory, that match files and
-# directories to ignore when looking for source files.
-# This pattern also affects html_static_path and html_extra_path.
 exclude_patterns = [
     "examples",
     "README*",
@@ -60,35 +43,31 @@ exclude_patterns = [
     "img/README.md",
 ]
 
+# Enable MyST extensions (including LaTeX math support)
+myst_enable_extensions = [
+    "amsmath",    # AMS LaTeX math environments
+    "dollarmath", # Inline and block math with $...$ and $$...$$
+]
 
 # -- Options for HTML output -------------------------------------------------
 
-# The theme to use for HTML and HTML Help pages.  See the documentation for
-# a list of builtin themes.
-#
 html_theme = "sphinx_rtd_theme"
 
-# Add any paths that contain custom static files (such as style sheets) here,
-# relative to this directory. They are copied after the builtin static files,
-# so a file named "default.css" will overwrite the builtin "default.css".
-#html_static_path = ['css']
-
-
-# HTML context:
+# HTML context
 from os.path import basename, dirname, realpath
 
 html_context = {
     "display_github": True,
     "github_user": github_user,
-    # Auto-detect directory name.  This can break, but
-    # useful as a default.
     "github_repo": github_repo_name or basename(dirname(realpath(__file__))),
     "github_version": github_version,
     "conf_py_path": conf_py_path,
 }
 
-import os
-if os.environ.get('GITHUB_REF', '') == 'refs/heads/'+github_version:
+if os.environ.get('GITHUB_REF', '') == 'refs/heads/' + github_version:
     html_js_files = [
-        ('https://plausible.cs.aalto.fi/js/script.js', {"data-domain": "coderefinery.github.io", "defer": "defer"}),
+        (
+            'https://plausible.cs.aalto.fi/js/script.js',
+            {"data-domain": "coderefinery.github.io", "defer": "defer"}
+        ),
     ]
